e799d1cdb4088979d63841633d56091727785a60
Jeffrey Davis <davisj0214@yahoo.com>
2019-08-15 18:55:58 -0500

    spotify, movie, and bandsintown complete

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # liri-node-app
+
+### Description
+**Liri** is the language equivalent of **Siri**.  Instead of speaking a command, we use text to ask for information.  In this app, you will use the command line to ask **Liri** about songs from Spotify, venues for a particular band or artist, or search for information on your favorite movies.  
+
+### Instructions
+1. Navigate to the command line
+2. Run the following command 
+   - node liri.js ... followed by one of the following
+     - concert-this <artist/band name here>
+     - spotify-this-song <song name here>
+     - movie-this <movie name here>
+
+You can even run the following command to see the information from the default text file
+- node liri.js do-what-it-says
--- liri.js
+++ liri.js
@@ -1,76 +1,99 @@
-require("dotenv").config();
+require('dotenv').config();
 
 var fs = require('fs');
-var request = require('request');
-var keys = require("./keys.js");
-var axios = require("axios");
-var spotifyNode = require('node-spotify-api');
-var spotify = new spotify(keys.spotify);
+var axios = require('axios');
+var keys = require('./keys.js');
+var moment = require('moment');
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
 var command = process.argv[2];
-var parameter = process.argv.splice[3];
-var searchParameter = parameter.join('+');
+var parameter = process.argv.slice(3).join(' ');
 
-function concertSearch(search) {
-
-    var queryURL = "https://rest.bandsintown.com/artists/" + "/event?app_id=codingbootcamp";
-
-    request(queryURL, function(err, response, body) {
-        if (!err && response.statusCode === 200) {
-            console.log('Top 5 Venues Available:');
-
-            var concertData = JSON.parse(body);
-            for (i = 0; i < 5; i++) {
-                console.log("Event Location: " + concertData[i].venue.name);
-                console.log("Country: " + concertData[i].venue.country);
-                console.log("City: " + concertData[i].venue.city + ", " + concertData[i].venue.region);
-
-                var date = new Date(concertData[i].datetime);
-                console.log("Time: " + date);
-                
-            }
+function spotifySearch(song) {
+    if (!song) {
+        song = 'The Sign by Ace of Base';
+    } else {
+        song = parameter;
+    }
+    spotify.search({
+        type: 'track',
+        query: song
+    }, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
         }
+        console.log('================================');
+        console.log('Artist: ' + data.tracks.items[0].artists[0].name);
+        console.log('Song: ' + data.tracks.items[0].name);
+        console.log('Preview: ' + data.tracks.items[0].preview_url);
+        console.log('Album: ' + data.tracks.items[0].album.name);
+        console.log('================================');
     })
 }
 
-function spotifySearch(search) {
-    spotify.search(
-        {
-            type: "track", 
-            query: search,
-            limit: 1
-        }) .then(function(response) {
-                var spotifyData = response.tracks.items[0];
-                
-                console.log("Song: " + spotifyData.name);
-                console.log("Artist: " + spotifyData.artists[0].name);
-                console.log("Album: " + spotifyData.album.name);
-                console.log("Preview Link: " + spotifyData.preview_url);
-        }) .catch(function(error) {
-            console.log(error);
-        })
+function concertSearch() {
+    axios.get(`https://rest.bandsintown.com/artists/${parameter}/events?app_id=codingbootcamp`).then(
+        function (response) {
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                console.log('================================');
+                console.log('Venue: ' + results[i].venue.name);
+                console.log('City: ' + results[i].venue.city);
+                console.log('Date: ' + moment(results[i].datetime).format('L'));
+                console.log('================================');
+            }
+        }
+    )
 }
 
-function movieSearch(search) {
-    var queryURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + search;
+function movieSearch(movie) {
+    if (!movie) {
+        movie = 'Mr. Nobody';
+    } else {
+        movie = parameter;
+    }
+    axios.get(`http://www.omdbapi.com/?t=${movie}&y=&plot=short&apikey=trilogy`).then(
+        function (response) {
+            console.log('================================');
+            console.log('Title: ' + response.data.Title);
+            console.log('Year Released: ' + response.data.Year);
+            console.log('IMDB Rating: ' + response.data.imdbRating);
+            console.log('Rotten Tomatoes: ' + response.data.Ratings[1].Value);
+            console.log('Country Produced: ' + response.data.Country);
+            console.log('Language: ' + response.data.Language);
+            console.log('Plot: ' + response.data.Plot);
+            console.log('Actors: ' + response.data.Actors);
+            console.log('================================');
+        }
+    )
+}
 
-    if (search === undefined) {
-        var search = "Mr.Nobody";
-        var queryURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + search;
-        console.log("You should watch Mr. Nobody!  It's on Netflix!");
+function doWhatItSays() {
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        var dataArray = data.split(',');
+        command = dataArray[0];
+        parameter = dataArray[1];
 
-        request(queryURL, function(err, response, body) {
-            if (!err && response.statusCode === 200) {
-                var movieData = JSON.parse(body);
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        
+        runApp();
+    })
+}
 
-                console.log("Title: " + movieData.Title);
-                console.log("Release Year: " + movieData.Year);
-                console.log("IMDB Rating: " + movieData.imdbRating);
-                console.log("Rotten Tomatoes: " + movieData.Ratings[1].Value);
-                console.log("Countrys: " + movieData.country);
-                console.log("Language: " + movieData.Language);
-                console.log("Plot: " + movieData.Plot);
-                console.log("Actors: " + movieData.Actors);
-            }
-        })
+function runApp() {
+    if (command === 'spotify-this-song') {
+        spotifySearch();
+    } else if (command === 'concert-this') {
+        concertSearch();
+    } else if (command === 'movie-this') {
+        movieSearch();
+    } else if (command === 'do-what-it-says') {
+        doWhatItSays();
     }
 }
+
+runApp();
